3d0fa398a782bd70c0d337c300876a98be630ed9
Thomas Cordua-von Specht <tcvs@endercrest.com>
2014-12-03 23:07:02 -0800

    First implementation of API stuff.

---
+++ src/main/java/com/endercrest/colorcube/api/PlayerLeaveArenaEvent.java
@@ -0,0 +1,23 @@
+package com.endercrest.colorcube.api;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PlayerLeaveArenaEvent extends Event implements Cancellable {
+    //TODO Implement this
+    @Override
+    public boolean isCancelled() {
+        return false;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return null;
+    }
+}
