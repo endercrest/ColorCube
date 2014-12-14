d769ae94c002425587dc058da226eeaa179e9211
Thomas Cordua-von Specht <tcvs@endercrest.com>
2014-12-13 22:46:33 -0800

    Unfreeze players after 5 seconds

--- src/main/java/com/endercrest/colorcube/powerups/Freeze.java
+++ src/main/java/com/endercrest/colorcube/powerups/Freeze.java
@@ -1,18 +1,32 @@
 package com.endercrest.colorcube.powerups;
 
+import com.endercrest.colorcube.ColorCube;
 import com.endercrest.colorcube.PowerupManager;
 import com.endercrest.colorcube.game.Game;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.List;
 
 public class Freeze implements SubPowerup {
     @Override
     public void onRightClick(Player p, Game g) {
+        final List<Player> playerList = g.getActivePlayers();
         PowerupManager.getInstance().addFrozenPlayers(g.getActivePlayers());
         PowerupManager.getInstance().removeFrozenPlayer(p);
+
+        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+        scheduler.scheduleSyncDelayedTask(ColorCube.getPlugin(), new Runnable() {
+            @Override
+            public void run() {
+                PowerupManager.getInstance().removeFrozenPlayers(playerList);
+            }
+        }, 100L);
     }
 
     @Override
