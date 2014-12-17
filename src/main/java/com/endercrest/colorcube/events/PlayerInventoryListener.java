0e86790e2d8ebaf333dde10c2f556f576270ea6f
Thomas Cordua-von Specht <tcvs@endercrest.com>
2014-12-16 18:24:14 -0800

    Disable inventory click.

---
+++ src/main/java/com/endercrest/colorcube/events/PlayerInventoryListener.java
@@ -0,0 +1,27 @@
+package com.endercrest.colorcube.events;
+
+import com.endercrest.colorcube.GameManager;
+import com.endercrest.colorcube.game.Game;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class PlayerInventoryListener implements Listener {
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event){
+        if(event.getWhoClicked() instanceof Player){
+            Player p = (Player)event.getWhoClicked();
+            int id = GameManager.getInstance().getPlayerGameID(p);
+            if(id != -1){
+                if(GameManager.getInstance().getGame(id).getStatus() == Game.Status.INGAME) {
+                    if(p.getGameMode() != GameMode.CREATIVE) {
+                        event.setCancelled(true);
+                    }
+                }
+            }
+        }
+    }
+}
