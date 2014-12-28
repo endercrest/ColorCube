96e8ef4f18a81bc43fe2e41b22af41eb72c31f6b
Thomas Cordua-von Specht <tcvs@endercrest.com>
2014-12-28 13:51:12 -0800

    Fix Issue(https://github.com/endercrest/ColorCube/issues/30)

--- src/main/java/com/endercrest/colorcube/events/PlayerRespawnListener.java
+++ src/main/java/com/endercrest/colorcube/events/PlayerRespawnListener.java
@@ -5,27 +5,25 @@ import com.endercrest.colorcube.SettingsManager;
 import com.endercrest.colorcube.game.Game;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class PlayerRespawnListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerRespawn(PlayerRespawnEvent event) {
         Player p = event.getPlayer();
-        if (GameManager.getInstance().isPlayerActive(p)) {
-            int id = GameManager.getInstance().getPlayerGameID(p);
-            Game game = GameManager.getInstance().getGame(id);
-            for (int spawnID : game.getSpawns().keySet()) {
-                if (game.getSpawns().get(spawnID).equals(p)) {
-                    if (game.getStatus() == Game.Status.INGAME) {
-                        event.setRespawnLocation(SettingsManager.getInstance().getSpawnPoint(id, spawnID));
-                        return;
-                    } else if (game.getStatus() == Game.Status.LOBBY || game.getStatus() == Game.Status.STARTING) {
-                        event.setRespawnLocation(GameManager.getInstance().getGame(id).getLobbySpawn());
-                        return;
+        if(GameManager.getInstance().isPlayerActive(p)){
+            Game game = GameManager.getInstance().getGame(GameManager.getInstance().getPlayerGameID(p));
+            if(game.getStatus() == Game.Status.INGAME){
+                for(int id: game.getSpawns().keySet()){
+                    if(game.getSpawns().get(id).getName().equals(p.getName())){
+                        event.setRespawnLocation(SettingsManager.getInstance().getSpawnPoint(game.getGameID(), id));
                     }
                 }
+            }else if(game.getStatus() == Game.Status.LOBBY || game.getStatus() == Game.Status.STARTING){
+                event.setRespawnLocation(game.getLobbySpawn());
             }
         }
     }
