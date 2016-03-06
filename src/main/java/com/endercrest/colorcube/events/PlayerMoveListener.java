4432aa65f10207b0ec17f3285e2caa94b19808d5
Thomas Cordua-von Specht <tcvs@endercrest.com>
2016-03-05 16:54:40 -0800

    Fix Error

--- src/main/java/com/endercrest/colorcube/events/PlayerMoveListener.java
+++ src/main/java/com/endercrest/colorcube/events/PlayerMoveListener.java
@@ -41,9 +41,10 @@ public class PlayerMoveListener implements Listener {
                 paintable.remove("AIR");
                 if(paintable.contains(loc.getBlock().getType().toString())){
                     if(loc.getBlock().getType().equals(Material.STAINED_CLAY)) {
-                        if()
-                        if (loc.getBlock().getData() != (byte) 15) {
-                            game.changeBlock(loc, teamID);
+                        if(game.getArena().containsBlock(loc)) {
+                            if (loc.getBlock().getData() != (byte) 15) {
+                                game.changeBlock(loc, teamID);
+                            }
                         }
                     }else{
                         game.changeBlock(loc, teamID);
